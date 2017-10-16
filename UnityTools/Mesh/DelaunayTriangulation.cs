705208b2c6c7d2524840a9cda7c4091fb2eeef18
Copper <siutungwu@gmail.com>
2017-10-16 18:24:28 +0800

    add mesh creator and delaunay triangulation

---
+++ UnityTools/Mesh/DelaunayTriangulation.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTools.Mesh {
+
+	/// <summary>
+	/// 3D Delaunay Triangulation Algorithm.
+	/// Reference from:
+	/// http://answers.unity3d.com/questions/307544/mesh-from-vertices-polygon-from-spline.html
+	/// http://gamedev.stackexchange.com/questions/60630/how-do-i-find-the-circumcenter-of-a-triangle-in-3d
+	/// </summary>
+	public class DelanunayTriangulation {
+
+		/// <summary>
+		/// Calculate the triangles which satisfied the rules of Delanunay Triangulation.
+		/// More information can be checked in:
+		/// https://en.wikipedia.org/wiki/Delaunay_triangulation
+		/// </summary>
+		public static int[] Calculate(Vector3[] vertices) {
+
+			if (vertices.Length == 0) {
+				return new int[1];
+			}
+
+			int[] result;
+			int i, j, k;
+
+			if (vertices.Length <= 3) {
+				result = new int[vertices.Length];
+				for (i = 0; i < vertices.Length; i++) {
+					result [i] = i;
+				}
+				return result;
+			}
+
+			List<int[]> triangles = new List<int[]> ();
+			int[] triangle;
+
+			// find all combinations of triangles without repetition
+			for (i = 0; i < vertices.Length - 2; i++) {
+				for (j = i + 1; j < vertices.Length - 1; j++) {
+					for (k = j + 1; k < vertices.Length; k++) {
+						triangle = new int[] { i, j, k };
+						triangles.Add (triangle);
+					}
+				}
+			}
+			Debug.Log ("Before Delaunay Conditioning: " + triangles.Count);
+			for (i = triangles.Count - 1; i >= 0; i--) {
+				if (!delaunayCondition (triangles [i], vertices)) {
+					triangles.RemoveAt (i);
+				}
+			}
+			Debug.Log ("After Delaunay Conditioning: " + triangles.Count);
+			result = new int[triangles.Count * 3];
+			for (i = 0; i < triangles.Count; i++) {
+				triangle = triangles [i];
+				if (i != triangles.Count - 1) {
+					result [i * 3] = triangle [0];
+					result [i * 3 + 1] = triangle [1];
+					result [i * 3 + 2] = triangle [2];
+				} else {
+					result [i * 3] = triangle [1];
+					result [i * 3 + 1] = triangle [2];
+					result [i * 3 + 2] = triangle [0];
+				}
+			}
+
+			return result;
+
+		}
+
+		private static bool delaunayCondition(int[] triangle, Vector3[] vertices) {
+
+			// 3 points of the triangle
+			Vector3 a = vertices [triangle [0]];
+			Vector3 b = vertices [triangle [1]];
+			Vector3 c = vertices [triangle [2]];
+
+			// circumcenter in 3D
+			Vector3 ac = c - a;
+			Vector3 ab = b - a;
+			Vector3 abXac = Vector3.Cross (ab, ac);
+			// vector from a to the circumcenter center
+			Vector3 toCircumsphereCenter = (Vector3.Cross (abXac, ab) * ac.sqrMagnitude + Vector3.Cross (abXac, ac) * ab.sqrMagnitude) / (2f * abXac.sqrMagnitude);
+			Vector3 circumsphereCenter = a + toCircumsphereCenter;
+			float circumsphereRadius = toCircumsphereCenter.magnitude;
+			// check if there is any vertices exists in the circumsphere, by definition, the circumsphere must not contains any vertices other than the defined three
+			for (int i = 0; i < vertices.Length; i++) {
+				if (i != triangle [0] && i != triangle [1] && i != triangle [2] && Vector3.Distance(circumsphereCenter, vertices[i]) <= circumsphereRadius) {
+					return false;
+				}
+			}
+			return true;
+
+		}
+
+	}
+
+}
---
+++ UnityTools/Mesh/MeshCreator.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.Mesh {
+
+	/// <summary>
+	/// Reference from:
+	/// http://catlikecoding.com/unity/tutorials/procedural-grid/
+	/// http://catlikecoding.com/unity/tutorials/rounded-cube/
+	/// </summary>
+	[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer)), ExecuteInEditMode]
+	public class MeshCreator : MonoBehaviour {
+
+		public class MeshLevel {
+
+			public bool foldout = true;
+			public List<Vector3> vertices;
+			public float radius;
+
+			public void clear() {
+
+				vertices.Clear ();
+
+			}
+
+		}
+
+		public int level = 1;
+		public List<MeshLevel> levels;
+
+		private UnityEngine.Mesh mesh;
+		private List<Vector3> vertices;
+		private bool generated = false;
+
+		void OnGizmosDraw() {
+
+			if (vertices == null || vertices.Count == 0) {
+				return;
+			}
+			// draw vertices
+			Gizmos.color = Color.black;
+			for (int i = 0; i < vertices.Count; i++) {
+				Gizmos.DrawSphere (vertices [i], 0.1f);
+			}
+
+		}
+
+		public IEnumerator generate() {
+
+			// create mesh
+			GetComponent<MeshFilter>().mesh = mesh = new UnityEngine.Mesh();
+			mesh.name = "Procedural Mesh";
+
+			// doing stuffs
+			for (int i = 0; i < level; i++) {
+				initLevel (i);
+			}
+			updateMesh ();
+			generated = true;
+
+			yield return null;
+
+		}
+
+		public void clear() {
+
+			generated = false;
+			vertices.Clear ();
+			GetComponent<MeshFilter>().mesh = mesh = null;
+
+		}
+
+		public void initLevel(int level) {
+
+			if (levels == null) {
+				levels = new List<MeshLevel> ();
+			}
+			if (levels.Count == level) {
+				MeshLevel newLevel = new MeshLevel ();
+				newLevel.vertices = new List<Vector3> ();
+				newLevel.radius = 1;
+				levels.Add (newLevel);
+				updateLevel (level, 3);
+			}
+
+		}
+
+		public void updateLevel(int level, int numOfVertices) {
+
+			if (numOfVertices < 3) {
+				numOfVertices = 3;
+			}
+			levels [level].clear ();
+			// verticesInLevel [level].Add (transform.position);
+			for (int i = 0; i < numOfVertices; i++) {
+				Vector3 newVertex = transform.position + levels [level].radius * new Vector3 (Mathf.Sin (360f / numOfVertices * i * Mathf.Deg2Rad), Mathf.Cos (360f / numOfVertices * i * Mathf.Deg2Rad));
+				levels [level].vertices.Add (newVertex);
+			}
+			if (generated) {
+				updateMesh ();
+			}
+
+		}
+
+		public void updateMesh() {
+
+			mesh.Clear ();
+			if (vertices == null) {
+				vertices = new List<Vector3> ();
+			} else if (vertices.Count > 0) {
+				vertices.Clear ();
+			}
+			for (int i = 0; i < levels.Count; i++) {
+				for (int j = 0; j < levels [i].vertices.Count; j++) {
+					vertices.Add (levels [i].vertices [j]);
+				}
+			}
+			mesh.vertices = vertices.ToArray ();
+			mesh.triangles = DelanunayTriangulation.Calculate (mesh.vertices);
+
+		}
+
+	}
+
+	[CustomEditor(typeof(MeshCreator))]
+	public class MeshCreatorEditor : Editor {
+
+		MeshCreator script;
+
+		void OnEnable() {
+
+			if (script == null)
+				script = (MeshCreator)target;
+
+		}
+
+		void OnDisable() {
+
+		}
+
+		public override void OnInspectorGUI() {
+
+			GUILayout.Label ("Settings", EditorStyles.boldLabel);
+			// variables
+			GUILayout.BeginHorizontal ();
+			GUILayout.Label ("Total Level: ");
+			script.level = EditorGUILayout.IntField (script.level);
+			GUILayout.EndHorizontal ();
+			for (int i = 0; i < script.level; i++) {
+				if (script.levels == null || script.levels.Count == i) {
+					script.initLevel (i);
+				}
+				script.levels [i].foldout = EditorGUILayout.Foldout (script.levels [i].foldout, "Level " + i);
+				if (script.levels [i].foldout) {
+					GUILayout.BeginHorizontal ();
+					GUILayout.Label ("Radius: ");
+					script.levels [i].radius = EditorGUILayout.FloatField (script.levels [i].radius);
+					GUILayout.EndHorizontal ();
+					GUILayout.BeginHorizontal ();
+					GUILayout.Label ("Verices Number ");
+					if (GUILayout.Button ("-")) {
+						script.updateLevel (i, script.levels [i].vertices.Count - 1);
+					}
+					GUILayout.Label (script.levels [i].vertices.Count.ToString ());
+					if (GUILayout.Button ("+")) {
+						script.updateLevel (i, script.levels [i].vertices.Count + 1);
+					}
+					GUILayout.EndHorizontal ();
+				}
+			}
+			// Buttons
+			GUILayout.BeginHorizontal ();
+			if (GUILayout.Button ("Generate")) {
+				script.StartCoroutine (script.generate ());
+			}
+			if (GUILayout.Button ("Clear")) {
+				script.clear ();
+			}
+			GUILayout.EndHorizontal ();
+			if (GUI.changed)
+				EditorUtility.SetDirty (script);
+
+		}
+
+	}
+
+}
