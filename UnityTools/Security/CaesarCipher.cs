739b2d20f7ef94c089b472d8e5e1275bfc844703
Copper <siutungwu@gmail.com>
2017-11-21 18:29:20 +0800

    add xor and caesar cipher

---
+++ UnityTools/Security/CaesarCipher.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace UnityTools.Security {
+
+	/// <summary>
+	/// Caesar cipher.
+	/// Reference:
+	/// https://en.wikipedia.org/wiki/Caesar_cipher
+	/// </summary>
+	public class CaesarCipher {
+
+		/// <summary>
+		/// Using Caesar Cipher to encrypt the specified string with a number.
+		/// </summary>
+		public static string Encrypt(string toEncrypt, int shiftNum) {
+
+			StringBuilder builder = new StringBuilder ();
+			for (int i = 0; i < toEncrypt.Length; i++) {
+				char temp = toEncrypt [i];
+				if (temp >= 'a' && temp <= 'z') {
+					temp = (char)((temp - 'a' + shiftNum) % 26 + 'a');
+				}
+				else if (temp >= 'A' && temp <= 'Z') {
+					temp = (char)((temp - 'A' + shiftNum) % 26 + 'A');
+				}
+				builder.Append (temp);
+			}
+			return builder.ToString ();
+
+		}
+
+		/// <summary>
+		/// Using Caesar Cipher to decrypt the specified string with a number (must be same as the one used in encryption).
+		/// </summary>
+		public static string Decrypt(string toDecrypt, int shiftNum) {
+
+			StringBuilder builder = new StringBuilder ();
+			for (int i = 0; i < toDecrypt.Length; i++) {
+				char temp = toDecrypt [i];
+				if (temp >= 'a' && temp <= 'z') {
+					temp = (char)((temp - 'a' - shiftNum + 26) % 26 + 'a');
+				}
+				else if (temp >= 'A' && temp <= 'Z') {
+					temp = (char)((temp - 'A' - shiftNum + 26) % 26 + 'A');
+				}
+				builder.Append (temp);
+			}
+			return builder.ToString ();
+
+		}
+
+	}
+
+}
---
+++ UnityTools/Security/SimpleXOR.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace UnityTools.Security {
+
+	public class SimpleXOR {
+
+		/// <summary>
+		/// Using Simple XOR Encryption to encrypt or decrypt the specified text with provided key.
+		/// </summary>
+		public static string EncryptDecrypt(string text, string key) {
+
+			StringBuilder builder = new StringBuilder ();
+			for (int i = 0; i < text.Length; i++) {
+				char temp = (char)(text [i] ^ key [i % key.Length]);
+				builder.Append (temp);
+			}
+			return builder.ToString ();
+
+		}
+
+	}
+
+}
